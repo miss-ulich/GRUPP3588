4d4275572b94aceb1dc2755908175e9ac874e269
Olesia <miss-ulich@yandex.ru>
2022-12-04 19:18:50 +1000

    add solution for task 0,1,3,5,7

---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if(inputLine!=null)
+{
+    // //Создаем массив с днями недели
+    // string[] dayofWeek = new string[7];
+    // dayOfWeek[0] = "понедельник";
+    // dayOfWeek[1] = "вторник";
+    // dayOfWeek[2] = "среда";
+    // dayOfWeek[3] = "четверг";
+    // dayOfWeek[4] = "пятница";
+    // dayOfWeek[5] = "суббота";
+    // dayOfWeek[6] = "воскресенье";
+        //Парсим введеное число
+        int inputNumber = int.Parse(inputLine);
+    // //Находим название дня недели по введенному номеру
+    //string outDayOfWeek = dayOfWeek[inputNumber-1];
+    // switch (inputNumber)    
+    // {
+    //      case1: outDayOfWeek = "понедельник"; break;
+    //      case2: outDayOfWeek = "вторник"; break;
+    //      case3: outDayOfWeek = "среда"; break;
+    //      case4: outDayOfWeek = "четверг"; break;
+    //      case5: outDayOfWeek = "пятница"; break;
+    //      case6: outDayOfWeek = "суббота"; break;
+    //      case7: outDayOfWeek = "воскресенье"; break;
+    //      default: outDayOfWeek = "'это не день недели"; break;
+    // }
+
+string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-RU").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(inputNumber));
+
+        //Выводим данные в консоль
+        Console.WriteLine(outDayOfWeek);
+}
---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,26 @@
+﻿//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if(inputLine!=null)
+{
+
+    //Парсим введеное число
+    int inputNumber = int.Parse(inputLine);
+
+    int startMumber = inputNumber*(-1);
+    
+    //Выходное значение
+    string outLine = string.Empty;
+
+    while(startMumber<inputNumber)
+    {
+        outLine = outLine + startMumber + ",";
+        startMumber++;
+        //startNumber=startNumber+1;
+    }
+    outLine = outLine + inputNumber;
+
+    //Выводим данные в консоль
+        Console.WriteLine(outLine);
+}
