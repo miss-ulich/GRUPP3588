28df89c50f1171f50aeb4d8ba89212ab6bf47589
Olesia <miss-ulich@yandex.ru>
2022-12-10 17:30:57 +1000

    add correction Sem2 Task9

--- Sem2Task9/Program.cs
+++ Sem2Task9/Program.cs
@@ -4,30 +4,30 @@ System.Random numberSintezator = new System.Random();
 
 //Вариант1
 //Получаем новое случайное число
-int number = numberSintezator.Next(10, 100);
+//int number = numberSintezator.Next(10, 100);
 //Выводим данные в консоль
 //Console.WriteLine(number);
 //Получаем первое и второе число поотдельности
-int firstNumber = number/10;
-int secondNumber = number%10;
+//int firstNumber = number/10;
+//int secondNumber = number%10;
 //Сравниваем числа
-if (firstNumber > secondNumber)
-{
+//if (firstNumber > secondNumber)
+//{
     //Выводим данные в консоль
     //Console.WriteLine(firstNumber);
-}
-else
-{
+//}
+//else
+//{
     //Выводим данные в консоль
     //Console.WriteLine(secondNumber);
-}
+//}
 
 //Вариант2
 char[] digits = numberSintezator.Next(10, 100).ToString().ToCharArray();
 Console.WriteLine(digits);
 
-firstNumber = ((int)digits[0])-48;
-secondNumber = ((int)digits[1])-48;
+int firstNumber = ((int)digits[0])-48;
+int secondNumber = ((int)digits[1])-48;
 
 int resultNumber = firstNumber > secondNumber? resultNumber = firstNumber : resultNumber = secondNumber;
 
