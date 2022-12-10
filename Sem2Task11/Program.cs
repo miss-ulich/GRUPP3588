1ba0c90e013ce3f6c5d9169ac1ec6026dba44d76
Olesia <miss-ulich@yandex.ru>
2022-12-10 17:00:05 +1000

    add solution for task 9, 11, 12, 14,16

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,11 @@
+﻿//Создаем экземпляр класса рандомайзер
+System.Random numberSintezator = new System.Random();
+
+//Получаем новое случайное число
+int num = numberSintezator.Next(100, 1000);
+Console.WriteLine (num);
+
+//int result = (num/100)*10 + num%10;
+
+//Выводим данные в консоль
+Console.WriteLine((num/100)*10 + num%10);
