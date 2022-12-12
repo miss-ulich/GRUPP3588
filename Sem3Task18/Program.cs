96c44a410d622da76ce08bc53c40e6135fb772d9
Olesia <miss-ulich@yandex.ru>
2022-12-12 19:04:55 +1000

    add task for solution 17, 18, 20, 22

---
+++ Sem3Task18/Program.cs
@@ -0,0 +1,22 @@
+﻿int quter = ReadData("Введите номер четверти");
+PrintAnswer(quter);
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод определяет четверть по координатам точки
+void PrintAnswer(int num)
+{
+    if (num > 0 && num < 5)
+    {
+        if (num == 1) Console.WriteLine("X > 0, Y > 0");
+        if (num == 2) Console.WriteLine("X > 0, Y < 0");
+        if (num == 3) Console.WriteLine("X < 0, Y < 0");
+        if (num == 4) Console.WriteLine("x < 0, Y > 0");
+    }
+    else Console.WriteLine("Вы ввели не номер четверти!");
+}
