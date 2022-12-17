9f4902d7a126f55013653b6fac6689b7639dc3e3
Olesia <miss-ulich@yandex.ru>
2022-12-17 20:03:03 +1000

    add solution for task 24, 26, 28, 30

---
+++ Sem4Task28/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача No28
+//Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+//Например: 4 -> 24
+//5 -> 120
+
+using System.Numerics;
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+BigInteger CalcFactor(int num)
+{    
+    BigInteger count = 1;
+    for(int i=1;i<=num;i++)
+    {
+       count *= i;
+    }
+    return count;
+}
+
+int number = ReadData("Введите число А:");
+
+BigInteger length1 = CalcFactor(number);
+
+PrintData("Факториал равно^ " + length1);
