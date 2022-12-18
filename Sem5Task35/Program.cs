aea061aa19c5b8199a9b2d26a66cf23f61269181
Olesia <miss-ulich@yandex.ru>
2022-12-18 16:25:19 +1000

    add solution for task 31, 32, 33, 35, 37

---
+++ Sem5Task35/Program.cs
@@ -0,0 +1,49 @@
+﻿//Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+//Например:
+//Пример для массива из 5, а не 123 элементов. В своем решении сделайте для 123
+//[5, 18, 123, 6, 2] -> 1 [1, 2, 3, 6, 2] -> 0
+//[10, 11, 12, 13, 14] -> 5
+ 
+//Заполняет массив
+int[] array = GenArray(123,-100,100);
+//Печатает
+PrintArray(array);
+int result = CountElement(array,10,99);
+Console.WriteLine();
+PrintData("Количество элементов массива, значения которых лежат в отрезке [10,99] составляет", result);
+
+int[] GenArray(int len, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[] outArray = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        outArray[i] = new Random().Next(minValue, maxValue);
+    }
+    return outArray;
+}
+void PrintData(string res, int value)
+{
+    Console.WriteLine(res + value);
+}
+void PrintArray(int[] arr) 
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ",");
+    }
+    Console.Write(arr[arr.Length - 1] + "]");
+}
+int CountElement(int[] array, int min, int max)
+{
+    int res = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > min && array[i] < max)
+        {
+            res++;
+        }
+    }
+    return res;
+}
---
+++ Sem5Task37/Program.cs
@@ -0,0 +1,43 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+//Например:
+//[1 2 3 4 5] -> 5 8 3
+//[6 7 3 6] -> 36 21
+
+
+int[] array = GenArray(123,-100,100);
+PrintArray(array);
+Console.WriteLine();
+int[] result = ConvertArr(array);
+PrintArray(result);
+Console.WriteLine();
+
+int[] GenArray(int len, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[] outArray = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        outArray[i] = new Random().Next(minValue, maxValue);
+    }
+    return outArray;
+}
+
+void PrintArray(int[] arr) 
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ",");
+    }
+    Console.Write(arr[arr.Length - 1] + "]");
+}
+
+int[] ConvertArr(int[] array)
+{
+    int[] outArr = new int[array.Length/2];
+    for (int i = 0; i < array.Length/2; i++)
+    {
+        outArr[i]=array[i]*array[array.Length-1 -i];
+    }
+    return outArr;
+}
